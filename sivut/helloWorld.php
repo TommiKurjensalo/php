e301061a2c17ff6430c99ad6a8e59d88b870abba
Tommi Kurjensalo <tommi.kurjensalo@myy.haaga-helia.fi>
2017-04-05 18:37:30 +0300

    HelloWorld version 2

--- sivut/helloWorld.php
+++ sivut/helloWorld.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html> 
  <html> 
- <head> 
- <meta charset="UTF-8"> 
- <title>Esimerkki</title> 
- </head> 
+  <head> 
+   <meta charset="UTF-8"> 
+    <title>Hei maailma!</title> 
+	<style>
+		.wrapper { 
+		font-size:10em;
+		margin:1em;
+		padding:0.1em;
+		text-align:center;
+		background:grey;
+		}
+	</style>
+  </head> 
 <body> 
-<?php
-
-	print("<p>Hello World!</p>");
-?>
+	<div class="wrapper">
+	<?php
+		print("<p>Hello World!</p>");
+	?>
+	</div>
 </body> 
 </html>
